cd3071d8a5130b00a123c30c39be53ecbe408315
Oracle Public Cloud User <opc@instance-cicd-project1.sub07191622530.cicdvcn.oraclevcn.com>
2024-08-02 20:01:59 +0000

    Test files

---
+++ .bash_history
@@ -0,0 +1,45 @@
+npm i
+sudo install nodejs
+sudo yum install -y nodejs
+sudo yum install git
+npm install express
+sudo node server.js
+cd "Server"
+sudo node server.js
+cd
+cd "Server"
+sudo node server.js
+cd server
+cd Server
+node server.js
+cd "Server"
+ls
+node server.js
+sudo node server.js
+cd Server
+sudo node server.js
+cd Server
+sudo node server.js
+sudo npm server.js
+cd
+ls
+sudo yum nodemon
+sudo yum npm
+sudo apt install npm
+sudo npm install -g npm
+cd Server
+sudo npm server.js
+ls
+npm server.js
+sudo npm
+cd
+cd Server
+sudo npm start
+ls
+cd
+sudo npm start
+cd Server
+sudo npm start
+npm start
+sudo npm
+sudo node server.js
---
+++ .vscode-server/data/User/History/-76110e33/3r3i.js
@@ -0,0 +1,48 @@
+document.addEventListener('DOMContentLoaded',function(){
+    const apiKey='085a2baf803d961034e4af8cf025ff07'
+    const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
+    
+
+    const weatherForm = document.getElementById("weatherForm");
+    const weatherInfo = document.getElementById("weatherInfo");
+
+   if(weatherForm &&weatherInfo){
+    weatherForm.addEventListener('submit',function(event){
+        event.preventDefault();
+        const cityNameInput = document.getElementById('cityNameInput');
+        if(cityNameInput) {
+            const cityName = cityNameInput.value;
+            fetchWeatherByCity(cityName);
+        }else{
+            console.error('City Name input element not found')
+        }
+    });
+   }else{
+    console.error("Weather form or element not found");
+   }
+
+    function fetchWeatherByCity(cityName){
+        const apiUrl = `${baseUrl}?q=${cityName}&appid=${apiKey}&units=imperial`;
+
+        fetch(apiUrl)
+            .then(response => {
+                if(!response.ok){
+                    throw new Error("Network response was not ok")
+                }
+                return response.json();
+            })
+            .then(data => {
+                const temperature= data.main.temp;
+                const description=data.weather[0].description;
+                const cityName=data.name;
+
+                weatherInfo.innerHTML= `<h2>Weather in ${cityName}</h2>
+                                        <p>Temperature: ${temperature} °C</p>
+                                        <p>Description: ${description}</p>`;
+            })
+            .catch(error => {
+                console.error("Fetch Error",error)
+                weatherInfo.innerHTML= `<p>Failed to fetch weather data for ${city}. Please Try Again. </p>`
+            });
+    }
+})
