4da7f261ee74927467a025bf35519d0e69419fc6
Oracle Public Cloud User <opc@instance-cicd-project1.sub07191622530.cicdvcn.oraclevcn.com>
2024-08-07 02:51:54 +0000

    updated jest

--- .vscode-server/data/User/History/-630f0bfb/j4XV.js
+++ .vscode-server/data/User/History/-630f0bfb/j4XV.js
@@ -1,9 +1,6 @@
 const {JSDOM}= require('jsdom')
-
 const{document}= (new JSDOM('')).window;
+
 global.document=document;
 global.window=document.defaultView;
 global.HTMLElement= window.HTMLElement;
-
-const fetch = require("node-fetch");
-global.fetch=fetch
